8e4bab9070a2865c9847d705fd00d239a564ca17
Ergun Polat <epolat@rocketlawyer.co.uk>
2016-11-28 14:40:24 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+//regiser middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+//maintenance middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+//handlebar helper
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+
+
+// app.get('/', (req, res) => {
+//   // res.send('<h1>Hello express</h1>');
+//   res.send({
+//     name:'Ergun',
+//     likes: ['Blue','Fitness']
+//   })
+// });
+
+
+ app.get('/', (req, res) => {
+    res.render('home.hbs', {
+      welcomeMessage: 'Welcome',
+      pageTitle: 'Home page'
+    });
+ });
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: 'Error handling request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on 3000');
+});
